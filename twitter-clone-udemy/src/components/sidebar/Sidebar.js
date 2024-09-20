b4b4b56f38c6e23abb938779395c220ef3b1fc6b
mituki1234 <144220463+mituki1234@users.noreply.github.com>
2024-09-20 07:26:02 +0000

    sidebarの基礎を作った

--- twitter-clone-udemy/src/components/sidebar/Sidebar.js
+++ twitter-clone-udemy/src/components/sidebar/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import TwitterIcon from '@mui/icons-material/Twitter';
 import SidebarOption from './SidebarOption';
+import HomeIcon from '@mui/icons-material/Home';
 
 function Sidebar() {
   return (
@@ -9,12 +10,13 @@ function Sidebar() {
       <TwitterIcon className='sidebar--twitterIcon'/>
 
       {/* sidebaroption */}
-      <SidebarOption />
-      {/* sidebaroption */}
-      {/* sidebaroption */}
-      {/* sidebaroption */}
-      {/* sidebaroption */}
-      {/* sidebaroption */}
+      <SidebarOption text='ホーム' Icon={HomeIcon}/>
+      <SidebarOption text='話題を検索' Icon={HomeIcon}/>
+      <SidebarOption text='通知' Icon={HomeIcon}/>
+      <SidebarOption text='メッセージ' Icon={HomeIcon}/>
+      <SidebarOption text='ブックマーク' Icon={HomeIcon}/>
+      <SidebarOption text='プロフィール' Icon={HomeIcon}/>
+      <SidebarOption text='もっと見る' Icon={HomeIcon}/>
 
       {/* tweetbutton */}
     </div>
